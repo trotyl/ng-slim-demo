9ef792c2891f834af07133f7dd5308f9b0e5fcb8
Trotyl <trotyl@qq.com>
2017-10-31 15:57:27 +0800

    implement app logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './todo'
+
+const STORAGE_KEY = 'todos';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
+  templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  todos: Todo[];
+
+  ngOnInit(): void {
+    this.todos = this.restore();
+  }
+
+  create(input: HTMLInputElement): void {
+    this.todos.push({
+      title: input.value,
+      completed: false,
+    })
+    input.value = '';
+    this.save();
+  }
+
+  toggle(todo: Todo): void {
+    todo.completed = !todo.completed;
+    this.save();
+  }
+
+  remove(todo: Todo): void {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+    this.save();
+  }
+
+  private restore(): Todo[] {
+    const todosJson = localStorage.getItem(STORAGE_KEY);
+    if (todosJson) {
+      return JSON.parse(todosJson);
+    }
+    return [];
+  }
+
+  private save(): void {
+    const todosJson = JSON.stringify(this.todos);
+    localStorage.setItem(STORAGE_KEY, todosJson);
+  }
 }
